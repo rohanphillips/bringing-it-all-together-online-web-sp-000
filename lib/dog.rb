035be8e9153d80dc191ccce55331a096ff86071c
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-21 17:28:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,4 +19,14 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+
 end
